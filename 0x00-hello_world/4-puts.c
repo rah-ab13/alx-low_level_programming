e50921f52e11b3d73eb5ae116474e5d8e4f19b39
rah-ab13 <rah.abdu001@gmail.com>
2022-09-09 07:11:10 -0700

    coding

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,5 +6,6 @@
 * description: using the main fuction                                       
 * this program prints "Programming is like building a multilingual puzzle   
 *  Return: 0                                                                 
-*/ 
-int main (void{                                                                 	               puts("\"Programming is like building a multilingual puzzle");   		                          return(0);                                                                                        }                          
+*/                                                                            
+int main(void)
+{	puts("\"Programming is like building a multilingual puzzle");   		                          return(0);                                                                                        }                          
