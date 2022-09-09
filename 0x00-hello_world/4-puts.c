b4bf47eca5ce977c154e632756e19727ed0d9a6a
rah-ab13 <rah.abdu001@gmail.com>
2022-09-09 06:49:51 -0700

    coding

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,10 @@
-#include <studio.h>
+#include <stdio.h>
 
 /**
- * main - print the string in the put function
- * 
- * Description: using the main function
- * this program prints "programming is like building a multilingual puzzle
- * Return: 0
- */
-int main (void)
-{
-       puts("\"Programming is like building a multilingual puzzle");
-  return (0);       
+* main - print the string in the put function  
+*                              
+* description: using the main fuction                                       
+* this program prints "Programming is like building a multilingual puzzle   
+*  Return: 0                                                                 
+*/ 
+int main (void{                                                                 	               puts("\"Programming is like building a multilingual puzzle");   		                          return(0);                                                                                        }                          
