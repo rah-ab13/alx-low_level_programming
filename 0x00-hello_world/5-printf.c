e1e8a04304ddcdbdd86b87c8d46d54ee7316b907
rah-ab13 <rah.abdu001@gmail.com>
2022-09-12 05:29:03 -0700

    Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,11 +4,11 @@
 * main - print the string in the put function
 *
 * Description: using the main function
-* this program prints "Programming is like building a multilingual puzzle
+* this program prints "with proper grammar, but the outcome is a piece of art
 * Return: 0
 */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
