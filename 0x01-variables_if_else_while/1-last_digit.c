79937693ed82710b267564dd0b8d8bdf773596a6
rah-ab13 <rah.abdu001@gmail.com>
2022-09-12 08:58:01 -0700

    The last digit

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,64 @@
+#include <stdlib.h>                                                                                                         
+
+#include <time.h>                                                                                                           
+
+#include <stdio.h>                                                                                                          
+
+/**                                                                                                                         
+
+* main - print if the number is positive, zero, negative                                                                    
+
+*                                                                                                                           
+
+* Description: using the main function                                                                                      
+
+* this programming prints "will assign a random number to the variable n                                                    
+
+* Return: 0                                                                                                                 
+
+*/                                                                                                                          
+
+int main(void)                                                                                                              
+
+{                                                                                                                           
+
+int n;                                                                                                                      
+
+int l;                                                                                                                      
+
+                                                                                                                            
+
+srand(time(0));                                                                                                             
+
+n = rand() - RAND_MAX / 2;                                                                                                  
+
+1 = n % 10;                                                                                                                 
+
+                                                                                                                            
+
+if (l > 5)                                                                                                                  
+
+{                                                                                                                           
+
+        printf("last digit of %d and is greater than 5\n", n, l);                                                           
+
+}                                                                                                                           
+
+else if (l == 0)                                                                                                            
+
+{                                                                                                                           
+
+        printf("last digit of %d and is 0\n", n, l);                                                                        
+
+}                                                                                                                           
+
+else                                                                                                                        
+
+{                                                                                                                           
+
+        printf("last digit of %d and is less than 6 and not 0\n", n, l);                                                    
+
+}                                                                                                                           
+
+return (0);                                                                                                                 
+
