e000b93c17c856aebbc427f59261ce602cc8b242
rah-ab13 <rah.abdu001@gmail.com>
2022-09-12 07:19:35 -0700

    the size of various types on the computer

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -19,6 +19,6 @@ int main(void)
 	printf("size of an int: %ld byte(s)\n", sizeof(i));
 	printf("size of a long int: %ld byte(s)\n", sizeof(li));
 	printf("size of a long long int: %ld byte(s)\n", sizeof(lli));
-	printf("size of a float: %ld byte(s)\n", sizeoffloat(f));
+	printf("size of a float: %ld byte(s)\n", sizeof(f));
 	return (0);
 }
