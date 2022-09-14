ef131c52d5bbf0e3bc9078ef58ed0fe2980a7e5a
rah-ab13 <rah.abdu001@gmail.com>
2022-09-14 12:00:52 -0700

    98 Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,15 +1,17 @@
-#include <stdio>
+
+
+#include <stdio.h>
+
 /**
  * print_to_98 - prints all natural numbers from input to 98,
- *		 in order separated by a comma followed by a space.
+ * 		 in order separated by a comma followedby a space.
  * @n: The number to begin counting at.
  */
 void print_to_98(int n)
 {
-
 	if (n >= 98)
 	{
-		while (n > 98)
+		while (n >= 98)
 			printf("%d, ", n--);
 		printf("%d\n", n);
 	}
