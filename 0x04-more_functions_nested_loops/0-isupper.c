86f859b2e57f51d0e92048c891769ef93b2e3005
rah-ab13 <rah.abdu001@gmail.com>
2022-09-15 10:25:25 -0700

    checks for uppercase character

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
-* _isupper - check if a character is uppercase or not
-* @c: character to be tested
-* Return: 1 whether it is, 0 otherwise
+*_isupper - check if a character is uppercase or not
+*@c: character to be tested
+*Return: 1 whether it is, 0 otherwise
 */
 
 int _isupper(int c)
 {
-	if (c >= 65 && (C <= 90)
+	if ((c >= 'A') && (C <= 'Z'))
 	{
 		return (1);
 	}
