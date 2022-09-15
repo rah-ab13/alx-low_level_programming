9c3b9eb1e30faf594d7529ac752573f737ece03f
rah-ab13 <rah.abdu001@gmail.com>
2022-09-15 10:18:41 -0700

    checks for a digit

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -2,9 +2,9 @@
 #define MAIN_H
 
 /**
-* main - header file for prototypes
-* void prototypes (void)
-* int prototypes (int)
+*main - header file for prototypes
+*void prototypes (void)
+*int prototypes (int)
 */
 
 void print_numbers(void);
